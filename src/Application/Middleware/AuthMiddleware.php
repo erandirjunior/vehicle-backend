fef34eecc75b665043c2602cd70a7c57f860eb3b
Erandir Junior <aefs12junior@gmail.com>
2020-08-14 17:19:55 -0300

    Added middleware

---
+++ src/Application/Middleware/AuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SRC\Application\Middleware;
+
+use PlugRoute\Http\Request;
+use PlugRoute\Middleware\PlugRouteMiddleware;
+
+/**
+ * Class AuthMiddleware
+ * @package SRC\Application\Middleware
+ */
+class AuthMiddleware implements PlugRouteMiddleware
+{
+    /**
+     * @param Request $request
+     * @return Request
+     * @throws \Exception
+     */
+    public function handle($request): Request
+    {
+        try {
+            $token = $request->header('HTTP_AUTHORIZATION');
+            $key = "token_key";
+
+            \Firebase\JWT\JWT::decode($token, $key, ['HS256']);
+        } catch (\Exception $e) {
+            return $request->redirectToRoute('access');
+        }
+
+        return $request;
+    }
+}
