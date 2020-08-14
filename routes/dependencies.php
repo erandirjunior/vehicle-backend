2eac21555b6d7c1628670f6bc1d58a6b25a70e42
Erandir Junior <aefs12junior@gmail.com>
2020-08-14 15:02:52 -0300

    Organizing routes

--- routes/dependencies.php
+++ routes/dependencies.php
@@ -2,15 +2,24 @@
 
 use SRC\Domain\Brand\Interfaces\ContactFindRepository;
 
-$BrandRepository = new \SRC\Infrastructure\Repository\BrandRepository(
+$brandRepository = new \SRC\Infrastructure\Repository\BrandRepository(
+    (new \SRC\Infrastructure\Database\Connection())->getConnection()
+);
+$modelRepository = new \SRC\Infrastructure\Repository\ModelRepository(
     (new \SRC\Infrastructure\Database\Connection())->getConnection()
 );
 
 return [
-    \SRC\Domain\Brand\Interfaces\BrandCreateRepository::class => $BrandRepository,
-    \SRC\Domain\Brand\Interfaces\BrandFindAllRepository::class => $BrandRepository,
-    \SRC\Domain\Brand\Interfaces\BrandFindRepository::class => $BrandRepository,
-    \SRC\Domain\Brand\Interfaces\BrandDeleteRepository::class => $BrandRepository,
-    \SRC\Domain\Brand\Interfaces\BrandUpdateRepository::class => $BrandRepository,
-    \SRC\Domain\Brand\Interfaces\BrandValidator::class => new \SRC\Infrastructure\Validator\BrandValidator()
+    \SRC\Domain\Brand\Interfaces\BrandCreateRepository::class => $brandRepository,
+    \SRC\Domain\Brand\Interfaces\BrandFindAllRepository::class => $brandRepository,
+    \SRC\Domain\Brand\Interfaces\BrandFindRepository::class => $brandRepository,
+    \SRC\Domain\Brand\Interfaces\BrandDeleteRepository::class => $brandRepository,
+    \SRC\Domain\Brand\Interfaces\BrandUpdateRepository::class => $brandRepository,
+    \SRC\Domain\Brand\Interfaces\BrandValidator::class => new \SRC\Infrastructure\Validator\BrandValidator(),
+    \SRC\Domain\Model\Interfaces\ModelCreateRepository::class => $modelRepository,
+    \SRC\Domain\Model\Interfaces\ModelFindAllRepository::class => $modelRepository,
+    \SRC\Domain\Model\Interfaces\ModelFindRepository::class => $modelRepository,
+    \SRC\Domain\Model\Interfaces\ModelDeleteRepository::class => $modelRepository,
+    \SRC\Domain\Model\Interfaces\ModelUpdateRepository::class => $modelRepository,
+    \SRC\Domain\Model\Interfaces\ModelValidator::class => new \SRC\Infrastructure\Validator\ModelValidator()
 ];
--- routes/routes.php
+++ routes/routes.php
@@ -13,16 +13,29 @@ $route->options('/{anything}', function () {
 });
 
 $route->group(['prefix' => '/', 'namespace' => 'SRC\Application\Controller'], function ($route) {
-    $route->get('brands', '\\BrandFindAll@handler');
+    $route->group(['namespace' => '\\Brand'], function ($route) {
+        $route->get('brands', '\\BrandFindAll@handler');
 
-    $route->post('brands', '\\BrandCreate@handler');
+        $route->post('brands', '\\BrandCreate@handler');
 
-    $route->put('brands/{id:\d+}', '\\BrandUpdate@handler');
+        $route->put('brands/{id:\d+}', '\\BrandUpdate@handler');
 
-    $route->delete('brands/{id:\d+}', '\\BrandDelete@handler');
+        $route->delete('brands/{id:\d+}', '\\BrandDelete@handler');
 
-    $route->get('brands/{id:\d+}', '\\BrandFind@handler');
+        $route->get('brands/{id:\d+}', '\\BrandFind@handler');
+    });
+
+    $route->group(['namespace' => '\\Model'], function ($route) {
+        $route->get('models', '\\ModelFindAll@handler');
+
+        $route->post('models', '\\ModelCreate@handler');
+
+        $route->put('models/{id:\d+}', '\\ModelUpdate@handler');
+
+        $route->delete('models/{id:\d+}', '\\ModelDelete@handler');
+
+        $route->get('models/{id:\d+}', '\\ModelFind@handler');
+    });
 });
-
 
 $route->on($dependecies);
