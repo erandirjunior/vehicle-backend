97d3ba1ea4144906e6c37d5196d532b9274e7706
Erandir <aefs12junior@gmail.com>
2020-08-13 22:08:41 -0300

    Removed unnecessary method

--- src/Domain/Brand/Interfaces/BrandUpdateRepository.php
+++ src/Domain/Brand/Interfaces/BrandUpdateRepository.php
@@ -5,6 +5,4 @@ namespace SRC\Domain\Brand\Interfaces;
 interface BrandUpdateRepository
 {
     public function update(int $id, BrandBoundery $BrandBoundery): bool;
-
-    public function checkIfHasOtherBrandWithTheSameName(int $id, string $identifier): bool;
 }
