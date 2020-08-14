71f064a912466c0c01e62a79e878736e263f45e3
Erandir Junior <aefs12junior@gmail.com>
2020-08-14 17:21:33 -0300

    Added Authorization header

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
